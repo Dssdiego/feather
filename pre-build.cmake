afb6e83e3f44ab2673f5c0161e250ae01ccc3267
Diego Santos Seabra <dss-diego@hotmail.com>
2023-02-08 08:37:42 -0300

    added cmake stuff

---
+++ pre-build.cmake
@@ -0,0 +1,12 @@
+message("Compiling shaders...")
+file(GLOB SHADERS ${CMAKE_CURRENT_LIST_DIR}/src/engine/assets/shaders/*.frag ${CMAKE_CURRENT_LIST_DIR}/src/engine/assets/shaders/*.vert)
+foreach(shader ${SHADERS})
+    execute_process(COMMAND glslc -c ${shader} -o ${shader}.spv)
+endforeach()
+
+message("Copying assets to binary folder...")
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/src/engine/assets DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+
+message("Copying config files to binary folder...")
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/configs/elixir.ini DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/configs/steam_appid.txt DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
